9795b610ef482157c19954ebaa4133fc510cf662
yousuf <yousuf3592@gmail.com>
2020-08-21 11:10:40 +0530

    added Math questions

--- src/dataFile.js
+++ src/dataFile.js
@@ -10,110 +10,165 @@ export const studentsDB = [
 export const questionsAll = [
   {
     id: "1.",
-    question: (
-      <MathEq
-        math={String.raw`If $\int \frac{2cosx-sinx+\lambda }{cosx+sinx-2}dx$ = A ln $\left | cosx+sinx-2 \right |$+Bx+C. Then the ordered triplet A, B, &lambda; is`}
-      />
-    ),
+    question: "What is multiplicative inverse of 0",
 
-    optionA: (
-      <MathEq
-        math={String.raw`$\left ( \frac{1}{2},\frac{3}{2},-1 \right )$`}
-      />
-    ),
+    optionA: 0,
 
-    optionB: (
-      <MathEq
-        math={String.raw`$\left ( \frac{3}{2},\frac{1}{2},-1 \right )$`}
-      />
-    ),
+    optionB: 1,
 
-    optionC: (
-      <MathEq
-        math={String.raw`$\left ( \frac{1}{2},-1, \frac{-3}{2} \right )$`}
-      />
-    ),
+    optionC: -1,
 
-    optionD: (
-      <MathEq
-        math={String.raw`$\left ( \frac{3}{2},-1,\frac{1}{2} \right )$`}
-      />
-    ),
+    optionD: "Does not exist",
   },
   {
     id: "2.",
-    question: (
-      <MathEq
-        math={String.raw`If $y^{2}=x^{2}-x+1$ and $I_{n}=\int \frac{x^{n}}{y}dx$ and $AI_{3}+BI_{2}+CI_{1}=x^{2}y$ then the ordered triplet A,B,C is`}
-      />
-    ),
+    question: "What is Additive inverse of -2/5",
 
-    optionA: (
-      <MathEq
-        math={String.raw`$\left ( \frac{1}{2},\frac{-1}{2},-1 \right )$`}
-      />
-    ),
-    optionB: <MathEq math={String.raw`$\left ( 3,1,0 \right )$`} />,
-    optionC: <MathEq math={String.raw`$\left ( 1,-1,2 \right )$`} />,
-    optionD: <MathEq math={String.raw`$\left ( 3,\frac{-5}{2},2 \right )$`} />,
+    optionA: -2 / 5,
+
+    optionB: 2 / 5,
+    optionC: -5 / 2,
+
+    optionD: 5 / 2,
   },
   {
     id: "3.",
-    question: (
-      <MathEq
-        math={String.raw`$\int 4cos\left(x+\frac{\pi }{6} \right )cos2x$. $cos \left( cos\left( \frac{5\pi}{6}+x\right )\right ) dx$`}
-      />
-    ),
+    question: "Simplify (-7/18*15/-7)-(1*1/4)+(1/2*1/4)",
 
-    optionA: (
-      <MathEq
-        math={String.raw`$-\left(x+\frac{sin4x}{4}+\frac{sin2x}{2} \right )+c$`}
-      />
-    ),
-    optionB: (
-      <MathEq
-        math={String.raw`$-\left(x+\frac{sin4x}{4}+\frac{sin2x}{2} \right )+c$`}
-      />
-    ),
-    optionC: (
-      <MathEq
-        math={String.raw`$-\left(x-\frac{sin4x}{4}+\frac{sin2x}{2} \right )+c$`}
-      />
-    ),
-    optionD: (
-      <MathEq
-        math={String.raw`$-\left(x-\frac{sin4x}{4}+\frac{sin2x}{2} \right )+c$`}
-      />
-    ),
+    optionA: 17 / 24,
+
+    optionB: 24 / 17,
+
+    optionC: -17 / 24,
+
+    optionD: -24 / 17,
   },
   {
     id: "4.",
-    question: (
-      <MathEq
-        math={String.raw`If $\int \frac{e^{x-1}}{x^{2}-5x+4}\,2x\,dx$ = A F(x-1) + B F(x-4) + C and F(x)=$\int\frac{e^{x}}{x}\,dx$. then A & B ordered set is`}
-      />
-    ),
-    optionA: (
-      <MathEq math={String.raw`$\left(\frac{-2}{3},\frac{8}{3}\right)$`} />
-    ),
-    optionB: (
-      <MathEq math={String.raw`$\left(\frac{-2}{3},\frac{8e^{3}}{3}\right)$`} />
-    ),
-    optionC: (
-      <MathEq math={String.raw`$\left(\frac{8}{3},\frac{2}{3}\right)$`} />
-    ),
-    optionD: (
-      <MathEq
-        math={String.raw`$\left(\frac{-2}{3},\frac{-8e^{3}}{3}\right)$`}
-      />
-    ),
+    question: "What Number should be added to 7/12 to get 4/15",
+
+    optionA: -11 / 30,
+
+    optionB: 51 / 60,
+
+    optionC: 1 / 20,
+
+    optionD: -19 / 60,
   },
   {
     id: "5.",
-    question: <MathEq math={String.raw`What is $a^{x}+b^{x}$`} />,
-    optionA: "normal",
-    optionB: "text",
-    optionC: "here",
-    optionD: <MathEq math={String.raw`$a^{y}+b^{y}$`} />,
+    question: "Evaluate 2/5*(-3/7)-1/6*3/2+1/14*2*5",
+
+    optionA: 11 / 28,
+
+    optionB: 28 / 11,
+
+    optionC: -11 / 28,
+
+    optionD: -28 / 11,
+  },
+  {
+    id: "6.",
+    question:
+      "Find the smallest number by which 25200 should be multiplied so that the result is a perfect square",
+
+    optionA: 3,
+    optionB: 5,
+    optionC: 7,
+    optionD: 11,
+  },
+  {
+    id: "7.",
+    question: <MathEq math={String.raw`$\sqrt{248+\sqrt{52+\sqrt{144}}}$`} />,
+
+    optionA: 14,
+    optionB: 16,
+    optionC: 16.6,
+    optionD: 18.8,
+  },
+  {
+    id: "8.",
+    question: (
+      <MathEq
+        math={String.raw`$\frac{112}{\sqrt{196}}\times\frac{\sqrt{576}}{12} \timesfrac{\sqrt{256}}{8}$`}
+      />
+    ),
+
+    optionA: 8,
+    optionB: 12,
+    optionC: 16,
+    optionD: 32,
+  },
+
+  {
+    id: "9.",
+    question: <MathEq math={String.raw`$\sqrt{31684}$`} />,
+
+    optionA: 168,
+    optionB: 172,
+    optionC: 178,
+    optionD: 182,
+  },
+
+  {
+    id: "10.",
+    question: <MathEq math={String.raw`$\sqrt{0.01+\sqrt{0.0064}}$`} />,
+
+    optionA: 0.3,
+    optionB: 0.03,
+    optionC: <MathEq math={String.raw`$\sqrt{0.18}$`} />,
+    optionD: "None of these",
+  },
+  {
+    id: "11.",
+    question: <MathEq math={String.raw`$\sqrt{9216}+\sqrt{12544}$`} />,
+
+    optionA: 196,
+    optionB: 200,
+    optionC: 206,
+    optionD: 218,
+  },
+  {
+    id: "12.",
+    question: (
+      <MathEq
+        math={String.raw`If $\sqrt{15625}=125$, then the value of $(\sqrt{15625}+\sqrt{156.25}+\sqrt{1.5625})$ is`}
+      />
+    ),
+
+    optionA: 1.3875,
+    optionB: 13.785,
+    optionC: 138.75,
+    optionD: 156.25,
+  },
+  {
+    id: "13.",
+    question:
+      "Which smallest number must be added to 2203 to get a perfect square?",
+
+    optionA: 1,
+    optionB: 3,
+    optionC: 6,
+    optionD: 8,
+  },
+  {
+    id: "14.",
+    question:
+      "What is the smallest integer which when multiplied by 392, gives a perfect square?",
+
+    optionA: 2,
+    optionB: 3,
+    optionC: 5,
+    optionD: 6,
+  },
+  {
+    id: "15.",
+    question:
+      "The least number to be subtracted from 16800 to make it a pefect square?",
+
+    optionA: 249,
+    optionB: 159,
+    optionC: 169,
+    optionD: 219,
   },
 ];
