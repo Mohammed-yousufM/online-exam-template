696cd54cf1d747af3008689afc725f0ab762a9c8
yousuf <yousuf3592@gmail.com>
2020-09-26 20:47:24 +0530

    Math questions added

--- src/dataFile.js
+++ src/dataFile.js
@@ -26,214 +26,200 @@ export const studentsDB = [
 export const questionsAll = [
   {
     id: "1.",
-    question: "The standard form of quadratic equation is",
+    question: "Which one of the following equation is quadratic",
 
-    optionA: (
-      <MathEq math={String.raw`A) $ax^{2} \, + \, bx \,+\, c \,= \,0$`} />
-    ),
-    optionB: <MathEq math={String.raw`B) $ax \, + \, b \,= \,0$`} />,
-    optionC: (
-      <MathEq
-        math={String.raw`C) $ax^{3} \, + \, bx^{2} \,+\, cx \, + \, d \, = \,0$`}
-      />
-    ),
-    optionD: "D) None of these",
+    optionA: <MathEq math={String.raw`A) $11=-4x^{2}-x^{3}$`} />,
+    optionB: <MathEq math={String.raw`B) $\frac{-3}{y} -4 =0$`} />,
+    optionC: <MathEq math={String.raw`C) $n-3=4n$`} />,
+    optionD: <MathEq math={String.raw`D) $-3y^{2}-2y-7=0$`} />,
   },
 
   {
     id: "2.",
-    question: (
-      <MathEq
-        math={String.raw`In quadratic equation $ax^{2} \, + \, bx \,+\, c \,= \,0$`}
-      />
-    ),
+    question: "Which one of the following equation is not a quadratic",
 
-    optionA: <MathEq math={String.raw`A) $a,b,c \: \in  \: R, a=0$`} />,
-    optionB: <MathEq math={String.raw`B) $a,b,c \: \in  \: R, a \neq 0$`} />,
-    optionC: "C) Both",
-    optionD: "D) None of these",
+    optionA: <MathEq math={String.raw`A) $q^{2}-4=-3$`} />,
+    optionB: <MathEq math={String.raw`B) $y^{2}-4=11y$`} />,
+    optionC: <MathEq math={String.raw`C) $4-y=3$`} />,
+    optionD: <MathEq math={String.raw`D) $-3y^{2}-2y-7=0$`} />,
   },
 
   {
     id: "3.",
     question: (
       <MathEq
-        math={String.raw`The standard formula for finding the roots of quadratic equation $ax^{2} \, + \, bx \,+\, c \,= \,0$`}
+        math={String.raw`The factors of the quadratic equtaion: $x^{2}-x-12=0$ is`}
       />
     ),
 
-    optionA: (
-      <MathEq
-        math={String.raw`A) $x \, = \, \frac{b \pm \sqrt{b^{2}-4ac}}{2a} $`}
-      />
-    ),
-    optionB: (
-      <MathEq math={String.raw`B) $x \, = \, \frac{\sqrt{b^{2}-4ac}}{2a} $`} />
-    ),
-    optionC: (
-      <MathEq
-        math={String.raw`C) $x \, = \, \frac{-b \pm \sqrt{b^{2}-4ac}}{2a} $`}
-      />
-    ),
-    optionD: <MathEq math={String.raw`D) $x \, = \, \frac{-b  }{2a} $`} />,
+    optionA: "(x+3)(x+4)=0",
+    optionB: "(x+3)(x-4)=0",
+    optionC: "(x-3)(x-4)=0",
+    optionD: "(x-3)(x+4)=0",
   },
 
   {
     id: "4.",
     question: (
       <MathEq
-        math={String.raw`Sum of the roots of the quadratic equation $ax^{2} \, + \, bx \,+\, c \,= \,0$`}
+        math={String.raw`Standard form of quadratic equtaion: $7-4x=x^{2}$ is`}
       />
     ),
 
-    optionA: <MathEq math={String.raw`A) $\frac{c}{a}$`} />,
-    optionB: "B) 0",
-    optionC: "C) 1",
-    optionD: <MathEq math={String.raw`D) $\frac{-b}{a}$`} />,
+    optionA: <MathEq math={String.raw`A) $x^{2}+4x-7=0$`} />,
+    optionB: <MathEq math={String.raw`B) $x^{2}+4x+7=0$`} />,
+    optionC: <MathEq math={String.raw`C) $x^{2}-4x-7=0$`} />,
+    optionD: "D) None of these",
   },
 
   {
     id: "5.",
     question: (
       <MathEq
-        math={String.raw`Product of the roots of the quadratic equation $ax^{2} \, + \, bx \,+\, c \,= \,0$`}
+        math={String.raw`The value of the discriminant of $x^{2}+x+1=0$ is`}
       />
     ),
 
-    optionA: <MathEq math={String.raw`A) $\frac{c}{a}$`} />,
-    optionB: "B) 0",
-    optionC: "C) 1",
-    optionD: <MathEq math={String.raw`D) $\frac{-b}{a}$`} />,
+    optionA: "A) -3",
+    optionB: "B) -4",
+    optionC: "C) -2",
+    optionD: "D) -5",
   },
 
   {
     id: "6.",
     question: (
       <MathEq
-        math={String.raw`In a quadratic equation $ax^{2} \, + \, bx \,+\, c \,= \,0$ the discriminant D = `}
+        math={String.raw`The value of the discriminant of $x^{2}-3x+2=0$ is`}
       />
     ),
 
-    optionA: <MathEq math={String.raw`A) $b^{2}+4ac$`} />,
-    optionB: <MathEq math={String.raw`B) $b^{2}-4ac$`} />,
-    optionC: <MathEq math={String.raw`C) $b^{2} \times 4ac$`} />,
-    optionD: <MathEq math={String.raw`D) $-b^{2}-4ac$`} />,
+    optionA: "A) 2",
+    optionB: "B) 1",
+    optionC: "C) 0",
+    optionD: "D) 17",
   },
 
   {
     id: "7.",
     question: (
       <MathEq
-        math={String.raw`Find the value of $\sqrt{10+\sqrt{25+\sqrt{108+\sqrt{154+\sqrt{225}}}}}$`}
+        math={String.raw`If one root of the quadratic equation $kx^{2}-7x+12=0$ is 3, then find the value of k`}
       />
     ),
 
-    optionA: "A) 4",
-    optionB: "B) 5",
-    optionC: "C) 6",
-    optionD: "D) 7",
+    optionA: "A) 0",
+    optionB: "B) -1",
+    optionC: "C) 1",
+    optionD: "D) 5",
   },
 
   {
     id: "8.",
     question: (
-      <MathEq math={String.raw`If $ab=6$, $a+b=5$ then $a^{2}+b^{2}=$`} />
+      <MathEq
+        math={String.raw`Nature of roots of quadratic equation $y^{2}-4y-1=0$ is`}
+      />
     ),
 
-    optionA: "A) 12",
-    optionB: "B) 0",
-    optionC: "C) 1",
-    optionD: "D) 13",
+    optionA: "A) Real and Equal",
+    optionB: "B) Not Real",
+    optionC: "C) Cannot be determined",
+    optionD: "D) Real and Distinct",
   },
 
   {
     id: "9.",
-    question: "Add ab-bc, bc-ca,  ca-ab",
+    question: (
+      <MathEq
+        math={String.raw`Find the quadratic equation whose roots are $\frac{1}{2}$ and $\frac{-1}{2}$`}
+      />
+    ),
 
-    optionA: "A) 0",
-    optionB: "B) ab+bc+ca",
-    optionC: "C) ab-bc",
-    optionD: "D) -ab-bc-ca",
+    optionA: (
+      <MathEq math={String.raw`A) $x^{2}+\frac{1}{4}x+\frac{1}{4}=0$`} />
+    ),
+    optionB: (
+      <MathEq math={String.raw`B) $x^{2}-\frac{1}{4}x+\frac{1}{4}=0$`} />
+    ),
+    optionC: (
+      <MathEq math={String.raw`C) $x^{2}+\frac{1}{4}x-\frac{1}{4}=0$`} />
+    ),
+    optionD: <MathEq math={String.raw`D) $x^{2}-\frac{1}{4}=0$`} />,
   },
 
   {
     id: "10.",
-    question: (
-      <MathEq
-        math={String.raw`The value of the product $\left ( 4a^{2} \, + \, 3b \right ) \times \left ( 4a^{2} \, + \, 3b \right )$ at a=1, b=2 is`}
-      />
-    ),
+    question: "Find the quadratic equation whose roots are 3 and 10",
 
-    optionA: "A) 90",
-    optionB: "B) 100",
-    optionC: "C) 10",
-    optionD: "D) 1",
+    optionA: <MathEq math={String.raw`A) $x^{2}+13x+30=0$`} />,
+    optionB: <MathEq math={String.raw`B) $x^{2}-13x-30=0$`} />,
+    optionC: <MathEq math={String.raw`C) $x^{2}-13x+30=0$`} />,
+    optionD: <MathEq math={String.raw`D) $x^{2}+13x-30=0$`} />,
   },
 
   {
     id: "11.",
     question: (
-      <MathEq
-        math={String.raw`If $a-\frac{1}{a} \, = 2$, then $a^{2}+\frac{1}{a^{2}}=$`}
-      />
+      <MathEq math={String.raw`The values of a,b,c from $x^{2}-x-3=0$ is`} />
     ),
 
-    optionA: "A) 2",
-    optionB: "B) 4",
-    optionC: "C) 6",
-    optionD: "D) 8",
+    optionA: "A) 1,1,3",
+    optionB: "B) 1,-1,-3",
+    optionC: "C) -1,-1,-3",
+    optionD: "D) -1,1,-3",
   },
 
   {
     id: "12.",
+    question: (
+      <MathEq math={String.raw`Additive inverse of $\frac{1}{5}$ is`} />
+    ),
 
-    question: "What is multiplicative inverse of 0",
-    optionA: "A) 0",
-    optionB: "B) 1",
-    optionC: "C) -1",
-    optionD: "D) Does not exist",
+    optionA: <MathEq math={String.raw`A) $\frac{-1}{5}$`} />,
+    optionB: "B) 5",
+    optionC: <MathEq math={String.raw`C) $\frac{1}{5}$`} />,
+    optionD: "D) -5",
   },
 
   {
     id: "13.",
     question: (
       <MathEq
-        math={String.raw`Find $\left ( 2^{-1}+3^{-1}+4^{-1} \right )^{0}$`}
+        math={String.raw`$\sqrt{\frac{225}{729}}-\sqrt{\frac{25}{144}}=$`}
       />
     ),
 
-    optionA: "A) 1",
-    optionB: "B) -1",
-    optionC: <MathEq math={String.raw`C) $\frac{1}{30}$`} />,
-    optionD: "D) 0",
+    optionA: <MathEq math={String.raw`A) $\frac{15}{108}$`} />,
+    optionB: <MathEq math={String.raw`B) $\frac{108}{15}$`} />,
+    optionC: <MathEq math={String.raw`C) $\frac{15}{12}$`} />,
+    optionD: "D) 1",
   },
 
   {
     id: "14.",
     question: (
       <MathEq
-        math={String.raw`If a = 7, then the value of $-\left ( \frac{1-2a}{a-5} \right )$ is`}
+        math={String.raw`$\left (3^{-7}\div 3^{-10}\right ) \, \times \, 3^{-5}=$`}
       />
     ),
 
-    optionA: <MathEq math={String.raw`A) $\frac{-13}{2}$`} />,
-    optionB: <MathEq math={String.raw`B) $\frac{13}{2}$`} />,
-    optionC: <MathEq math={String.raw`C) $\frac{-15}{2}$`} />,
-    optionD: <MathEq math={String.raw`D) $\frac{15}{2}$`} />,
+    optionA: <MathEq math={String.raw`A) $\frac{1}{3}$`} />,
+    optionB: <MathEq math={String.raw`B) $\frac{1}{9}$`} />,
+    optionC: <MathEq math={String.raw`C) $\frac{1}{27}$`} />,
+    optionD: <MathEq math={String.raw`D) $\frac{1}{81}$`} />,
   },
 
   {
     id: "15.",
     question: (
-      <MathEq
-        math={String.raw`Find $x$, if $\left(\frac{5}{11} \right )^{-3} \times \, \left(\frac{5}{11} \right )^{5} \, = \, \left(\frac{5}{11} \right )^{x}$`}
-      />
+      <MathEq math={String.raw`$\left(3^{0}+4^{-1} \right )\times 2^{2}=$`} />
     ),
 
-    optionA: "A) 3",
+    optionA: <MathEq math={String.raw`A) $\frac{1}{5}$`} />,
     optionB: "B) 4",
-    optionC: "C) 2",
-    optionD: "D) 8",
+    optionC: "C) 5",
+    optionD: <MathEq math={String.raw`A) $\frac{1}{4}$`} />,
   },
 
   {
