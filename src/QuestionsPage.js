d768930f42a7f012ad105fc7074831c98331944a
yousuf <yousuf3592@gmail.com>
2020-10-21 20:47:18 +0530

    new server

--- src/QuestionsPage.js
+++ src/QuestionsPage.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 import { questionsAll } from "./dataFile";
 
@@ -6,7 +7,7 @@ class QuestionsPage extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { status: "" };
+    this.state = { status: "", name: this.props.username, submitting: false };
     this.i = 0;
     this.j = 0;
   }
@@ -28,25 +29,47 @@ class QuestionsPage extends Component {
     }
   }
 
-  submitForm = (ev) => {
+  submitOn = async (ev) => {
     ev.preventDefault();
-    const form = ev.target;
-    const data = new FormData(form);
-    const xhr = new XMLHttpRequest();
-    xhr.open(form.method, form.action);
-    xhr.setRequestHeader("Accept", "application/json");
-    xhr.onreadystatechange = () => {
-      if (xhr.readyState !== XMLHttpRequest.DONE) return;
-      if (xhr.status === 200) {
-        form.reset();
+    this.setState({ submitting: true });
+    try {
+      const res = await axios.post(
+        "https://knowledgefactory.herokuapp.com/users",
+        this.state
+      );
+      if (res.data === "success") {
+        ev.target.reset();
         this.setState({ status: "SUCCESS" });
         this.submitInfoToParent();
       } else {
-        this.setState({ status: "ERROR" });
+        this.setState({ submitting: false });
+        alert("Submission failed!\nPlease check your internet and try again!");
       }
-    };
-    xhr.send(data);
+    } catch (error) {
+      this.setState({ submitting: false });
+      alert("Submission failed!\nPlease check your internet and try again!");
+    }
   };
+
+  // submitForm = (ev) => {
+  //   ev.preventDefault();
+  //   const form = ev.target;
+  //   const data = new FormData(form);
+  //   const xhr = new XMLHttpRequest();
+  //   xhr.open(form.method, form.action);
+  //   xhr.setRequestHeader("Accept", "application/json");
+  //   xhr.onreadystatechange = () => {
+  //     if (xhr.readyState !== XMLHttpRequest.DONE) return;
+  //     if (xhr.status === 200) {
+  //       form.reset();
+  //       this.setState({ status: "SUCCESS" });
+  //       this.submitInfoToParent();
+  //     } else {
+  //       this.setState({ status: "ERROR" });
+  //     }
+  //   };
+  //   xhr.send(data);
+  // };
 
   mainQfunc = (que) => {
     return (
@@ -67,8 +90,11 @@ class QuestionsPage extends Component {
                 name={que["id"]}
                 defaultChecked={false}
                 value="A"
+                onClick={() => {
+                  this.setState({ [que["id"]]: "A" });
+                }}
               />
-              <label htmlFor={this.j}>{que["optionA"]}</label>
+              /<label htmlFor={this.j}>{que["optionA"]}</label>
             </div>
           </div>
           <div className="column">
@@ -79,6 +105,9 @@ class QuestionsPage extends Component {
                 name={que["id"]}
                 defaultChecked={false}
                 value="B"
+                onClick={() => {
+                  this.setState({ [que["id"]]: "B" });
+                }}
               />
               <label htmlFor={this.j}>{que["optionB"]}</label>
             </div>
@@ -93,6 +122,9 @@ class QuestionsPage extends Component {
                 name={que["id"]}
                 defaultChecked={false}
                 value="C"
+                onClick={() => {
+                  this.setState({ [que["id"]]: "C" });
+                }}
               />
               <label htmlFor={this.j}>{que["optionC"]}</label>
             </div>
@@ -105,6 +137,9 @@ class QuestionsPage extends Component {
                 name={que["id"]}
                 defaultChecked={false}
                 value="D"
+                onClick={() => {
+                  this.setState({ [que["id"]]: "D" });
+                }}
               />
               <label htmlFor={this.j}>{que["optionD"]}</label>
             </div>
@@ -117,12 +152,13 @@ class QuestionsPage extends Component {
   render() {
     const userID = this.props.username;
     const { status } = this.state;
+    // console.log(this.state);
     return (
       <form
         className="container pr-2"
         id="formSubmit"
         name="examForm"
-        // onSubmit={this.submitForm}
+        onSubmit={this.submitOn}
         // //insert your formspree integration endpoint into action attribute below
         // action="https://formspree.io/f/mqkgpvgk"
         // method="POST"
